600c79591739f6dfc6e18473eb06f41d409e8e2b
Student <student@ubuntu20.04>
2022-03-14 14:31:45 +0200

    Change main content

--- App/htdocs/index.php
+++ App/htdocs/index.php
@@ -13,6 +13,7 @@
 		<header>
 			<div class="title">
 				<h1>TEST CodeBuild</h1>
+				<h1>Terraform</h1>
 				<h1>Generation of pseudo-random passwords!</h1>
 				<p>
 				Generation of pseudo-random passwords based on entropy operating system based on the Linux kernel
